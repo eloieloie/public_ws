124dadcc8652760727df0459cac0a4d2bc7ce0e3
Eloi Elimelech <54350123+eloieloie@users.noreply.github.com>
2025-09-03 09:11:44 +0000

    update

--- chk_jdbc.java
+++ chk_jdbc.java
@@ -2,6 +2,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.DatabaseMetaData;
+import java.sql.Driver;
 import java.util.Properties;
 import java.io.FileInputStream;
 import java.io.IOException;
